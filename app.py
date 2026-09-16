523aacdf08ad2fe997aa7679324a18f9f4ac643d
mariamm-90 <MAriamm90@nycstudents.net>
2026-09-16 10:32:23 -0400

    5

--- app.py
+++ app.py
@@ -2,24 +2,58 @@ import turtle
 from turtle import *
 t = Turtle()
 
-t.shape('turtle')
-t.forward(200)
+# """ t.shape('turtle')
+# t.forward(200)
 
-def message(input):
-    print(input)
-message("Hello Class")
+# def message(input):
+#     print(input)
+# message("Hello Class")
 
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
+# def square(x):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+# square(200)
 
 
+# def equal(x):
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+# equal(200) 
+
+# def right():
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(135)
+#     t.forward(142)
+# right() """
+
+# """ def rectangle(x):
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(125)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(125)
+#     t.left(90)
+# rectangle(200) """
+
+def equal(x):
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(200)
 
 turtle.done ()
---
+++ loops.py
@@ -0,0 +1,3 @@
+for i in range(4):
+    t.forward(100)
+    t.left(90)
