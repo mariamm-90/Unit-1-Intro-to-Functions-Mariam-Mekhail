02395cdacdd535dc778e6a6021c568f3679131be
mariamm-90 <MAriamm90@nycstudents.net>
2026-09-23 10:04:04 -0400

    5

--- loops.py
+++ loops.py
@@ -2,17 +2,12 @@ import turtle
 from turtle import *
 t = Turtle()
 
-""" for i in range(4):
-    print(i)
+for i in range(60):
+    for j in range(4):
+        t.forward(100)
+        t.right(90)
+    t.right(5)
 
-for i in range(4):
-    t.forward(100)
-    t.left(90) """
 
-def square ():
-for i in range(4):
-    t.forward(100)
-    t.left(5)
 
-square()
 turtle.done()
