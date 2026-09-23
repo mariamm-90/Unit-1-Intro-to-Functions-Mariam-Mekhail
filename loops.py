e658b08c63a5e4c0ab58d191a25dc889c158a6b4
mariamm-90 <MAriamm90@nycstudents.net>
2026-09-23 19:47:13 -0400

    5

--- loops.py
+++ loops.py
@@ -1,25 +1,57 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(0)
 
-""" for i in range(60):
-    for j in range(4):
-        t.forward(100)
-        t.right(90)
-    t.right(5) """
 
+#defining square
 def square(length,angle):
     for _ in range (4):
         t.forward(length)
         t.right(angle)
 
+
+#function that draws 60 squares, turning right 5 degrees after each square
+def drawSpiral ():
+    for i in range(60):
+        square (100,90)
+        t.right(5)
+drawSpiral()
+
+
+#5 squares doubling in size each time
 def addSquares(iRange):
     length = 25
     for i in range(iRange):
-        square(length, 90)
+        square(length,90)
         length += 25
 addSquares(5)
 
+
+#function drawing 60 squares, turning 5 degrees after each square and making each successive square bigger
+def drawSpiral():
+    length = 5
+    for i in range(60):
+        square(length,90)
+        t.right(5)
+        length += 5
+drawSpiral()
+
+
+#defining star
+def star(length):
+    for _ in range(5):
+        t.forward(length)
+        t.right(144)
+
+#function drawing a 5 pointed star using the angle 144
+def starSpiral():
+    length = 5
+    for i in range(60):
+        star(length)
+        t.right(5)
+        length += 5
+starSpiral()
 
 
 turtle.done()
